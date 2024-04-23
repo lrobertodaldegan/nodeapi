6350d9d5f6b3af2b2d6ac868efb352d126ddb90f
lroberto <lroberto@tribanco.com.br>
2024-04-23 18:19:39 -0300

    Ajustes

--- app/controllers/ranking.controller.js
+++ app/controllers/ranking.controller.js
@@ -17,13 +17,13 @@ exports.list = (req, res) => {
   .populate('app')
   .then(itens => {
     if(itens && itens !== null){
-      let result = itens.sort(a, b => a.score > b.score);
+      let result = itens.sort((a, b) => b.score - a.score);
 
       res.status(200).send({app: req.appName, ranking: result});
     } else {
       res.status(204).send({app: req.appName, ranking: []});
     }
-  }).catch(err => errorHandler(ess, res));
+  }).catch(err => errorHandler(err, res));
 }
 
 exports.add = (req, res) => {
@@ -35,24 +35,25 @@ exports.add = (req, res) => {
   })
   .then(r => {
     if(r)
-      return res.status(200).send({message:'Operação realizada com sucesso!'});
+      return res.status(200).send({message:'OperaÃ§Ã£o realizada com sucesso!'});
 
     let rank = new Ranking({
       app: req.appId,
       player: req.body.player,
+      game: req.body.game,
       score: req.body.score
     });
 
     rank.save().then(newRank => {
-      res.status(200).send({message:'Operação realizada com sucesso!'});
-    }).catch(err => errorHandler(ess, res));
-  }).catch(err => errorHandler(ess, res));
+      res.status(200).send({message:'OperaÃ§Ã£o realizada com sucesso!'});
+    }).catch(err => errorHandler(err, res));
+  }).catch(err => errorHandler(err, res));
 }
 
 exports.remove = (req, res) => {
   Ranking.deleteOne({
     _id:req.params.id
   }).then(() => {
-    res.status(200).send({message:'Operação realizada com sucesso!'})
-  }).catch(err => errorHandler(ess, res));
+    res.status(200).send({message:'OperaÃ§Ã£o realizada com sucesso!'})
+  }).catch(err => errorHandler(err, res));
 }
